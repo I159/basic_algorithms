d8da494a237d3ed0abde05a60a22b7a14f416cf2
I159 <pekelny@gmail.com>
2013-06-19 00:02:31 +0300

    Deepcopy is changing algorythm behavior

--- map_coloring.py
+++ map_coloring.py
@@ -31,12 +31,6 @@ countries = {
             'relations': ['Equador', 'Peru', 'Brazil', 'Venezuella']},
         'Venezuella': {'color': '',
             'relations': ['Columbia', 'Brazil', 'Guyana']},
-        'Guyana': {'color': '',
-            'relations': ['Venezuella', 'Brazil', 'Suriname']},
-        'Suriname': {'color': '',
-            'relations': ['Guyana', 'Brazil', 'French Guiana']},
-        'French Guiana': {'color': '',
-            'relations': ['Brazil', 'Suriname']},
         'Galapogos Islands': {'color': '', 'relations': []},
         'Falkland Islands': {'color': '', 'relations': []},
         'South Georgia':  {'color': '', 'relations': []}}
@@ -45,63 +39,130 @@ countries = {
 colored_countries = {}
 
 
-def sort_countries():
-    sorted_keys = countries.keys()
-    sorted_keys.sort(
-            cmp=lambda x, y: len(countries[x]) - len(countries[y]),
-            reverse=True)
-    return sorted_keys
-
-
-def get_relevant_color(relations):
-    color_index = len(colors) - 1
-    for country in relations:
-        if countries[country]['color'] == colors[color_index]:
-            color_index -= 1
-    return colors[color_index]
-
-
-def set_colors():
-    sorted_countries = sort_countries()
-    country_index = 0
-    while country_index < len(sorted_countries):
-        country = countries[sorted_countries[country_index]]
-        if not country['color']:
-            if country_index == 0:
-                country['color'] = colors[0]
-            else:
-                country['color'] = get_relevant_color(country['relations'])
-        country_index += 1
-
-
-class TestMapColoring(object):
+class ColoringMap(object):
     def __init__(self):
-        set_colors()
+        # TODO: check what is wrong with deepcopy
+        self.countries = countries
+
+    def sort_countries(self):
+        sorted_keys = self.countries.keys()
+        sorted_keys.sort(
+                cmp=lambda x, y: len(countries[x]) - len(countries[y]),
+                reverse=True)
+        return sorted_keys
+
+    def get_relevant_color(self, relations):
+        color_index = len(colors) - 1
+        for country in relations:
+            if self.countries[country]['color'] == colors[color_index]:
+                color_index -= 1
+        return colors[color_index]
+
+    def set_colors(self):
+        sorted_countries = self.sort_countries()
+        country_index = 0
+        while country_index < len(sorted_countries):
+            country = self.countries[sorted_countries[country_index]]
+            if not country['color']:
+                if country_index == 0:
+                    country['color'] = colors[0]
+                else:
+                    country['color'] = self.get_relevant_color(
+                            country['relations'])
+            country_index += 1
+        return self.countries
+
+
+#def sort_countries():
+#    sorted_keys = countries.keys()
+#    sorted_keys.sort(
+#            cmp=lambda x, y: len(countries[x]) - len(countries[y]),
+#            reverse=True)
+#    return sorted_keys
+#
+#
+#def get_relevant_color(relations):
+#    color_index = len(colors) - 1
+#    for country in relations:
+#        if countries[country]['color'] == colors[color_index]:
+#            color_index -= 1
+#    return colors[color_index]
+#
+#
+#def set_colors():
+#    sorted_countries = sort_countries()
+#    country_index = 0
+#    while country_index < len(sorted_countries):
+#        country = countries[sorted_countries[country_index]]
+#        if not country['color']:
+#            if country_index == 0:
+#                country['color'] = colors[0]
+#            else:
+#                country['color'] = get_relevant_color(country['relations'])
+#        country_index += 1
+
+
+class TestMapColoringClass(object):
+    def __init__(self):
+        self.countries = ColoringMap().set_colors()
 
     def test_count_colors(self):
         colors_count = {}
-        for v in countries.itervalues():
+        for v in self.countries.itervalues():
             if v['color'] not in colors_count:
                 colors_count[v['color']] = 1
             else:
                 colors_count[v['color']] += 1
-        print '====================== COLORS SCHEME ======================== '
+        print '====================== CLASS COLORS SCHEME ======================== '
         print colors_count
 
     def test_map_cost(self):
         colors_count = {}
         map_cost = 0
-        for v in countries.itervalues():
+        for v in self.countries.itervalues():
             if v['color'] not in colors_count:
                 colors_count[v['color']] = 1
             else:
                 colors_count[v['color']] += 1
         for k, v in colors_count.iteritems():
             map_cost += (v * colors.index(k) * 100)
-        print '========================= MAP COST ==========================='
+        print '========================= CLASS MAP COST ==========================='
         print map_cost
 
     def test_country_coloring(self):
-        print '===================== COUNTRIES AS IS ===================='
-        for k, v in countries.iteritems():
+        print '===================== CLASS COUNTRIES AS IS ===================='
+        for k, v in self.countries.iteritems():
             print k, ': ', v
+
+
+#class TestMapColoring(object):
+#    def __init__(self):
+#        set_colors()
+#
+#    def test_count_colors(self):
+#        colors_count = {}
+#        for v in countries.itervalues():
+#            if v['color'] not in colors_count:
+#                colors_count[v['color']] = 1
+#            else:
+#                colors_count[v['color']] += 1
+#        print '====================== COLOR SCHEME ======================== '
+#        print colors_count
+#
+#    def test_map_cost(self):
+#        colors_count = {}
+#        map_cost = 0
+#        for v in countries.itervalues():
+#            if v['color'] not in colors_count:
+#                colors_count[v['color']] = 1
+#            else:
+#                colors_count[v['color']] += 1
+#        for k, v in colors_count.iteritems():
+#            map_cost += (v * colors.index(k) * 100)
+#        print '========================= MAP COST ==========================='
+#        print map_cost
+#
+#    def test_country_coloring(self):
+#        print '===================== COUNTRIES AS IS ===================='
+#        for k, v in countries.iteritems():
+#            print k, ': ', v
